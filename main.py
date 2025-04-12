5fe72668913757c175ea6f7fe4b1485a320d5a90
eggifarhan <eggifarhan@gmail.com>
2025-04-13 03:01:10 +0700

    feat: add /webhook endpoint

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Fly.io!"
+    return "Hello Eggi Farhan!"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+    print("📩 Received Webhook:", data)
+    return "Webhook received!", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
