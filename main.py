e596b7ab4e51d78c7b19f190dc3025148f7338c2
eggifarhan <eggifarhan@gmail.com>
2025-04-13 03:06:40 +0700

    Add /webhook POST handler

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    print("📩 Received Webhook")
+    print(request.json)  # tampilkan data dari TradingView
+    return "Webhook received", 200
+
 @app.route("/")
 def home():
     return "Hello Eggi Farhan!"
-
-@app.route("/webhook", methods=["POST"])
-def webhook():
-    data = request.json
-    print("📩 Received Webhook:", data)
-    return "Webhook received!", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
